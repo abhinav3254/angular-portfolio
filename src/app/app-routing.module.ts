2bd40d420f1c4325f3f2fe9fdefcd9c6d0986e19
abhinav3254 <abhinav3254@gmail.com>
2023-11-04 22:45:49 +0530

    submit form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
